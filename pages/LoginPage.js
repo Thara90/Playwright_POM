ea14a3c89e9c5903d06452c9078a67f65df97a8a
thara <tpshadinijk@gmail.com>
2023-05-14 23:51:43 +0530

    test data and login changes

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,19 +1,28 @@
-const { expect } = require ('@playwright/test');
+const { expect } = require('@playwright/test');
 
-exports.LoginPage = class LoginPage{
+exports.LoginPage = class LoginPage {
 
     constructor(page) {
         this.page = page;
         this.txtUsername = page.getByPlaceholder('Enter your username');
         this.txtPassword = page.getByPlaceholder('Enter your password');
         this.btnSignIn = page.getByRole('link', { name: 'Sign in' });
+        this.lblName = page.locator(`//div[@class='logged-user-name']`);
+        this.lblRole = page.locator(`//div[@class='logged-user-role']`);
     }
-    async GotoLoginPage(){
+    async GotoLoginPage() {
         await this.page.goto('https://demo.applitools.com/index.html');
     }
     async Login(username, password) {
         await this.txtUsername.fill(username);
         await this.txtPassword.fill(password);
         await this.btnSignIn.click();
+    }
+    async AssertLoggedInUserDetails(userDetails) {
+        var role = await this.lblRole.textContent();
+        var role = role.trim();
+        await expect(this.lblName).toContainText(userDetails.name);
+        await expect(this.lblRole).toContainText(role);
+
     }
 }
---
+++ tests/login.spec.js
@@ -0,0 +1,17 @@
+
+const { test } = require('../base/base.js');
+const testData = JSON.parse(JSON.stringify(require('../test-data/TestData.json')));
+
+test.describe('Login Test Suite', () => {
+
+    test('Logint with valid credentials', async ({ loginPage }) => {
+        await loginPage.GotoLoginPage();
+        await loginPage.Login(testData.user.username, testData.user.password);
+        await loginPage.AssertLoggedInUserDetails(testData.user);
+    });
+
+    test.afterAll(async ({ page }) => {
+        await page.close();
+    });
+    
+});
